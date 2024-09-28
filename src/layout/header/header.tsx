3d03c2232af24f5b020861f49379c7c66e937a79
leonraz58 <leonraz58@gmail.com>
2024-09-28 23:08:17 +0300

    add header menu

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,7 +1,10 @@
 import s from './header.module.scss'
+import logo from './../../assets/images/logo.png'
+import {Menu} from "./menu/menu";
 
 export const Header = () => {
     return <header className={s.header}>
-        <div>Header</div>
+        <div><img src={logo} alt="logo" className={s.logo}/></div>
+        <Menu/>
     </header>
 }
---
+++ src/layout/header/menu/menu.tsx
@@ -0,0 +1,42 @@
+import {Link} from "react-router-dom";
+import s from './menu.module.scss'
+
+
+const items = [
+    {
+        title: 'О нас',
+        href: 'about'
+    },
+    {
+        title: 'Столы',
+        href: 'tables'
+    },
+    {
+        title: 'Контакты',
+        href: 'contacts'
+    },
+    {
+        title: 'Отзывы',
+        href: 'responses'
+    },
+    {
+        title: 'Заказать',
+        href: 'order'
+    }
+]
+
+export const Menu = () => {
+    return <nav >
+        <ul className={s.menu}>
+            {items.map((item, i) => {
+                return <li key={i}>
+                    <Link
+                        to={item.href}
+                    >
+                        {item.title}
+                    </Link>
+                </li>
+            })}
+        </ul>
+    </nav>
+}
