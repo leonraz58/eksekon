333229ee521cc81757ceb163d976eb7f8e66bf19
leonraz58 <leonraz58@gmail.com>
2024-09-28 23:57:49 +0300

    add contact icons

---
+++ src/layout/header/contacts/contacts.tsx
@@ -0,0 +1,17 @@
+import {Phone} from "../../../assets/icons/phone";
+import {Email} from "../../../assets/icons/email";
+import {Whatsapp} from "../../../assets/icons/whatsapp";
+import s from './contacts.module.scss'
+
+export const Contacts = () => {
+    return <div>
+        <ul className={s.contactList}>
+            <li><a href="tel:+79869369848"><Phone/></a></li>
+            <li><a href="mailto:EKSEKON.RU@YANDEX.RU"><Email/></a></li>
+            <li><a
+                href="https://wa.me/79869369848?text=%D0%9F%D0%B8%D1%88%D1%83%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%20Eksekon.ru"><Whatsapp/></a>
+            </li>
+
+        </ul>
+    </div>
+}
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,10 +1,14 @@
 import s from './header.module.scss'
 import logo from './../../assets/images/logo.png'
 import {Menu} from "./menu/menu";
+import {Contacts} from "./contacts/contacts";
 
 export const Header = () => {
     return <header className={s.header}>
-        <div><img src={logo} alt="logo" className={s.logo}/></div>
+        <div>
+            <img src={logo} alt="logo" className={s.logo}/>
+        </div>
         <Menu/>
+        <Contacts/>
     </header>
 }
