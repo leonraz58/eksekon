a880e7c0e5adea8ade64d9731c1e2a47119fb11e
leonraz58 <leonraz58@gmail.com>
2024-10-04 01:34:12 +0300

    add images preview to Colors picker

--- src/layout/chairs/card/colors/colors.tsx
+++ src/layout/chairs/card/colors/colors.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import s from "./colors.module.scss";
+import clsx from "clsx";
 
-// const colors = [
-//     {
-//         cssColor: 'red'
-//     },
-//     {
-//         cssColor: 'blue'
-//     },
-//     {
-//         cssColor: 'green'
-//     }
-// ]
 
 type Props = {
     colors: Array<string>
     currentColor: string
     setCurrentColor: (color: string) => void;
+    colorsImages: any
 }
 
-export const Colors = ({colors, setCurrentColor, currentColor}:Props) => {
+export const Colors = ({colors, setCurrentColor, currentColor, colorsImages}: Props) => {
     return (
-        <div className={s.colors}>
+        <div>
             Цвет: {currentColor}
-            {colors.map((item, index) => {
 
-                const onClickHandler = () => {
-                    setCurrentColor(item);
-                }
+            <div className={s.colors}>
 
-                return (<span onClick={onClickHandler} key={index} style={{backgroundColor: item}} className={s.color}>1</span>)
-            })}
+                {colors.map((item, index) => {
+
+                    const onClickHandler = () => {
+                        setCurrentColor(item);
+                    }
+
+                    let imgClassname = clsx(item === currentColor ? s.colorActive : '', s.colorImg)
+
+                    return (<img src={colorsImages[index]} onClick={onClickHandler} key={index}
+                                 className={imgClassname}></img>)
+                    // return (<span onClick={onClickHandler} key={index} style={{backgroundColor: item}} className={s.color}>1</span>)
+                })}
+
+            </div>
         </div>
     );
 };
