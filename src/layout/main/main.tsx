da21a03eb15c43f4a3876153bd65fa3e4def9842
leonraz58 <leonraz58@gmail.com>
2024-10-07 18:30:06 +0300

    fix main page wrappers

--- src/layout/main/main.tsx
+++ src/layout/main/main.tsx
@@ -2,20 +2,32 @@ import s from './main.module.scss'
 import AliceCarousel from "react-alice-carousel";
 import image from './../../assets/images/main.jpeg'
 import imageSlider from './../../assets/images/mainSlider.jpeg'
+import imageSlider2 from './../../assets/images/mainSlider2.jpeg'
+import "react-alice-carousel/lib/alice-carousel.css";
 import './../../styles/aliceCarousel.css'
+import {PageContainer} from "../../components/pageContainer/pageContainer";
 
 export const Main = () => {
 
     const items = [
         <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide1</h2></div>,
-        <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide2</h2></div>,
+        <div className={s.imgWrapper}><img className={s.image} src={imageSlider2} alt=""></img><h2>slide2</h2></div>,
         <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide3</h2></div>,
     ]
 
-    return <section className={s.sectionMain}>
-        {/*<div className={s.mainDiv}><img className={s.image} src={image} alt=""/></div>*/}
-        <div className={s.mainDiv}><AliceCarousel disableDotsControls={true} items={items}/></div>
-
+    return <section>
+        <div className={s.sectionMain}>
+            <PageContainer>
+                {/*<div className={s.mainDiv}><img className={s.image} src={image} alt=""/></div>*/}
+                <div className={s.mainDiv}>
+                    <AliceCarousel animationType={'fadeout'}
+                                   disableDotsControls={true}
+                                   items={items}
+                        //renderPrevButton={() => (<div>{'<'}</div>)}
+                    />
+                </div>
+            </PageContainer>
+        </div>
 
     </section>
 }
