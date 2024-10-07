91a37cf11a7a5969d5827858abd05581f3ad456b
leonraz58 <leonraz58@gmail.com>
2024-10-07 17:25:20 +0300

    add big slider

--- src/layout/main/main.tsx
+++ src/layout/main/main.tsx
@@ -1,13 +1,21 @@
 import s from './main.module.scss'
+import AliceCarousel from "react-alice-carousel";
+import image from './../../assets/images/main.jpeg'
+import imageSlider from './../../assets/images/mainSlider.jpeg'
+import './../../styles/aliceCarousel.css'
 
 export const Main = () => {
+
+    const items = [
+        <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide1</h2></div>,
+        <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide2</h2></div>,
+        <div className={s.imgWrapper}><img className={s.image} src={imageSlider} alt=""></img><h2>slide3</h2></div>,
+    ]
+
     return <section className={s.sectionMain}>
-            <div className={s.wrapper}>
-                <div>
-                    <h1 className={s.title1}>Eksekon</h1>
-                    <h2 className={s.title2}>Новый взгляд на дизайн мебели</h2>
-                </div>
-                <div className={s.form1}><span>Форма</span></div>
-            </div>
+        {/*<div className={s.mainDiv}><img className={s.image} src={image} alt=""/></div>*/}
+        <div className={s.mainDiv}><AliceCarousel disableDotsControls={true} items={items}/></div>
+
+
     </section>
 }
