f042ff23d346ad111b04f8aaf9715d9d4634e07c
leonraz58 <leonraz58@gmail.com>
2024-10-18 22:17:52 +0300

    add basketReducer

--- src/utils/basketReducer.tsx
+++ src/utils/basketReducer.tsx
@@ -7,14 +7,24 @@ export type BasketItem = {
 
 const initialState = [] as BasketItem[]
 
-export type AppInitialStateType = typeof initialState;
-
 const slice = createSlice({
     name: 'basket',
     initialState: initialState,
-    reducers: {setBasketItems: (state, action: PayloadAction< BasketItem[] >) => {
+    reducers: {
+        setBasketItems: (state, action: PayloadAction<BasketItem[]>) => {
+            localStorage.setItem('ids', JSON.stringify(action.payload));
             return action.payload;
-        },}
+        },
+        addItemToBasket(state, action: PayloadAction<BasketItem>) {
+            localStorage.setItem('ids', JSON.stringify([...state, action.payload]));
+            return ([...state, action.payload])
+        },
+        removeItemFromBasket(state, action: PayloadAction<string>) {
+            let finalState = state.filter((item) => item.id !== action.payload)
+            localStorage.setItem('ids', JSON.stringify(finalState));
+            return finalState
+        }
+    }
 })
 
 export const basketReducer = slice.reducer
