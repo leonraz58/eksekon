75a2371ca40530a385a24da330060f939e87c467
leonraz58 <leonraz58@gmail.com>
2024-10-18 22:33:02 +0300

    add store

--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,6 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {basketReducer} from "./basketReducer";
+import {appReducer} from "./appReducer";
 
 export const store = configureStore({
-    reducer: {basket: basketReducer},
+    reducer: {basket: basketReducer, app: appReducer},
 })
+
+export type AppRootStateType = ReturnType<typeof store.getState>
