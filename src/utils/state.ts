e6c18535676bbeb9a05fa883741fbb752e4bf3c1
leonraz58 <leonraz58@gmail.com>
2024-10-28 03:17:15 +0300

    add chairs

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -17,12 +17,6 @@ export type Chair = {
 
 export const chairs: Chair[] = [
     {
-        id: 'chbm1',
-        title: 'Дизайнерский стул в стиле Bold Moustache (Черный)',
-        description: info.chbm.description,
-        images: CHAIRS.chbm1
-    },
-    {
         id: 'benlines1',
         title: 'Дизайнерская лавка Lines (Сливочный+Манго 8 полос)',
         description: info.benlines.description,
@@ -33,5 +27,347 @@ export const chairs: Chair[] = [
         title: 'Дизайнерская лавка Lines (Графит 7 полос)',
         description: info.benlines.description,
         images: CHAIRS.benlines2
+    },
+    {
+        id: 'chbm1',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Черный)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm1
+    },
+    {
+        id: 'chbm2',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Малахитовый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm2
+    },
+    {
+        id: 'chbm3',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Корица)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm3
+    },
+    {
+        id: 'chbm4',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Манго)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm4
+    },
+    {
+        id: 'chbm5',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Нежно-розовый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm5
+    },
+    {
+        id: 'chbm6',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Фуксия)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm6
+    },
+    {
+        id: 'chbm7',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Нежно-серый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm7
+    },
+    {
+        id: 'chbm8',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Зеленый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm8
+    },
+    {
+        id: 'chbm9',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Кофе с молоком 2)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm9
+    },
+    {
+        id: 'chbm10',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Пшеничный)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm10
+    },
+    {
+        id: 'chbm11',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Сливочный)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm11
+    },
+    {
+        id: 'chbm12',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Марсала 2)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm12
+    },
+    {
+        id: 'chbm13',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Электрик)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm13
+    },
+    {
+        id: 'chbm14',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Розовый букле)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm14
+    },
+    {
+        id: 'chbm15',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Темно-бирюзовый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm15
+    },
+    {
+        id: 'chbm16',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Серый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm16
+    },
+    {
+        id: 'chbm17',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Сиреневый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm17
+    },
+    {
+        id: 'chbm18',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Мятный)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm18
+    },
+    {
+        id: 'chbm19',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Нежно-желтый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm19
+    },
+    {
+        id: 'chbm20',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Дымчатый голубой)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm20
+    },
+    {
+        id: 'chbm21',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Стальной 2)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm21
+    },
+    {
+        id: 'chbm22',
+        title: 'Дизайнерский стул в стиле Bold Moustache (Леопард коричневый)',
+        description: info.chbm.description,
+        images: CHAIRS.chbm22
+    },
+    {
+        id: 'chlines1',
+        title: 'Дизайнерский стул Lines (Трава)',
+        description: info.chlines.description,
+        images: CHAIRS.chlines1
+    },
+    {
+        id: 'chlines2',
+        title: 'Дизайнерский стул Lines (Манго)',
+        description: info.chlines.description,
+        images: CHAIRS.chlines2
+    },
+    {
+        id: 'chlines3',
+        title: 'Дизайнерский стул Lines (Нежно-серый)',
+        description: info.chlines.description,
+        images: CHAIRS.chlines3
+    },
+    {
+        id: 'chlines4',
+        title: 'Дизайнерский стул Lines (Сливочный)',
+        description: info.chlines.description,
+        images: CHAIRS.chlines4
+    },
+    {
+        id: 'chlines5',
+        title: 'Дизайнерский стул Lines (Черный)',
+        description: info.chlines.description,
+        images: CHAIRS.chlines5
+    },
+    {
+        id: 'chmol2v1',
+        title: 'Дизайнерский стул The Molecula Oxygen Синий (Version 2)',
+        description: info.chmol.description,
+        images: CHAIRS.chmol2v1
+    },
+    {
+        id: 'chmol2v2',
+        title: 'Дизайнерский стул The Molecula Oxygen Серый (Version 2)',
+        description: info.chmol.description,
+        images: CHAIRS.chmol2v2
+    },
+    {
+        id: 'chmol2v3',
+        title: 'Дизайнерский стул The Molecula Oxygen Красный (Version 2)',
+        description: info.chmol.description,
+        images: CHAIRS.chmol2v3
+    },
+    {
+        id: 'sbbm1',
+        title: 'Дизайнерский полубарный стул 62см в стиле Bold Moustache (Сливочный букле)',
+        description: info.sbbm.description,
+        images: CHAIRS.sbbm1
+    },
+    {
+        id: 'sbbm2',
+        title: 'Дизайнерский полубарный стул 62см в стиле Bold Moustache (Розовый букле)',
+        description: info.sbbm.description,
+        images: CHAIRS.sbbm2
+    },
+    {
+        id: 'sbbm3',
+        title: 'Дизайнерский полубарный стул 62см в стиле Bold Moustache (Бирюзовый букле)',
+        description: info.sbbm.description,
+        images: CHAIRS.sbbm3
+    },
+    {
+        id: 'tabbm1',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Корица)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm1
+    },
+    {
+        id: 'tabbm2',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Пыльно-розовый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm2
+    },
+    {
+        id: 'tabbm3',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Малахитовый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm3
+    },
+    {
+        id: 'tabbm4',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Черный)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm4
+    },
+    {
+        id: 'tabbm5',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Зеленый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm5
+    },
+    {
+        id: 'tabbm6',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Нежно-розовый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm6
+    },
+    {
+        id: 'tabbm7',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Нежно-серый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm7
+    },
+    {
+        id: 'tabbm8',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Фуксия)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm8
+    },
+    {
+        id: 'tabbm9',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Манго)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm9
+    },
+    {
+        id: 'tabbm10',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Пшеничный)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm10
+    },
+    {
+        id: 'tabbm11',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Марсала 2)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm11
+    },
+    {
+        id: 'tabbm12',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Розовая пудра)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm12
+    },
+    {
+        id: 'tabbm13',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Синий Электрик)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm13
+    },
+    {
+        id: 'tabbm14',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Серо-бирюзовый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm14
+    },
+    {
+        id: 'tabbm15',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Белый букле)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm15
+    },
+    {
+        id: 'tabbm16',
+        title: 'Дизайнерский табурет в стиле Bold Moustache (Леопард коричневый)',
+        description: info.tabbm.description,
+        images: CHAIRS.tabbm16
+    },
+    {
+        id: 'tablines1',
+        title: 'Дизайнерский табурет Lines (Трава)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines1
+    },
+    {
+        id: 'tablines2',
+        title: 'Дизайнерский табурет Lines (Манго)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines2
+    },
+    {
+        id: 'tablines3',
+        title: 'Дизайнерский табурет Lines (Нежно-серый)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines3
+    },
+    {
+        id: 'tablines4',
+        title: 'Дизайнерский табурет Lines (Нежно-серый + серый)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines4
+    },
+    {
+        id: 'tablines5',
+        title: 'Дизайнерский табурет Lines (Темно-бирюзовый)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines5
+    },
+    {
+        id: 'tablines6',
+        title: 'Дизайнерский табурет Lines (Стальной 2)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines6
+    },
+    {
+        id: 'tablines7',
+        title: 'Дизайнерский табурет Lines (Серый букле)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines7
+    },
+    {
+        id: 'tablines8',
+        title: 'Дизайнерский табурет Lines (Электрик)',
+        description: info.tablines.description,
+        images: CHAIRS.tablines8
     },
 ]
