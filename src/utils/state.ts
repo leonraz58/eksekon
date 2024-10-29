b81b1cc1f8287b085e2c70e65f9dae79e08fafbe
leonraz58 <leonraz58@gmail.com>
2024-10-29 16:18:24 +0300

    add state price

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -11,6 +11,10 @@ export type Chair = {
     images: string[]
     subtype: ChairType
     model: ChairModel
+    price: {
+        current: number
+        withoutDiscount: number
+    }
 }
 
 export const chairs: Chair[] = [
@@ -20,7 +24,11 @@ export const chairs: Chair[] = [
         description: info.benlines.description,
         images: CHAIRS.benlines1,
         subtype: 'bench',
-        model: 'lines'
+        model: 'lines',
+        price: {
+            current: 30000,
+            withoutDiscount: 32000
+        }
     },
     {
         id: 'benlines2',
@@ -28,7 +36,11 @@ export const chairs: Chair[] = [
         description: info.benlines.description,
         images: CHAIRS.benlines2,
         subtype: 'bench',
-        model: 'lines'
+        model: 'lines',
+        price: {
+            current: 27000,
+            withoutDiscount: 32000
+        }
     },
     {
         id: 'chbm1',
@@ -36,7 +48,11 @@ export const chairs: Chair[] = [
         description: info.chbm.description,
         images: CHAIRS.chbm1,
         subtype: 'chair',
-        model: 'magnet'
+        model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chbm2',
@@ -45,6 +61,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm2,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 22000
+        }
     },
     {
         id: 'chbm3',
@@ -53,6 +73,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm3,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm4',
@@ -61,6 +85,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm4,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 26000
+        }
     },
     {
         id: 'chbm5',
@@ -69,6 +97,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm5,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 17500,
+            withoutDiscount: 24000
+        }
     },
     {
         id: 'chbm6',
@@ -77,6 +109,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm6,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 23000
+        }
     },
     {
         id: 'chbm7',
@@ -85,6 +121,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm7,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm8',
@@ -93,6 +133,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm8,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm9',
@@ -101,6 +145,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm9,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm10',
@@ -109,6 +157,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm10,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm11',
@@ -117,6 +169,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm11,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 17500,
+            withoutDiscount: 21000
+        }
     },
     {
         id: 'chbm12',
@@ -125,6 +181,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm12,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm13',
@@ -133,6 +193,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm13,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm14',
@@ -141,6 +205,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm14,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm15',
@@ -149,6 +217,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm15,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm16',
@@ -157,6 +229,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm16,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm17',
@@ -165,6 +241,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm17,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm18',
@@ -173,6 +253,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm18,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm19',
@@ -181,6 +265,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm19,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm20',
@@ -189,6 +277,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm20,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm21',
@@ -197,6 +289,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm21,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chbm22',
@@ -205,6 +301,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chbm22,
         subtype: 'chair',
         model: 'magnet',
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chlines1',
@@ -213,6 +313,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chlines1,
         subtype: 'chair',
         model: "lines",
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chlines2',
@@ -221,6 +325,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chlines2,
         subtype: 'chair',
         model: "lines",
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chlines3',
@@ -229,6 +337,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chlines3,
         subtype: 'chair',
         model: "lines",
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chlines4',
@@ -237,6 +349,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chlines4,
         subtype: 'chair',
         model: "lines",
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chlines5',
@@ -245,6 +361,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chlines5,
         subtype: 'chair',
         model: "lines",
+        price: {
+            current: 15000,
+            withoutDiscount: 28000
+        }
     },
     {
         id: 'chmol2v1',
@@ -253,6 +373,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chmol2v1,
         subtype: 'chair',
         model: 'molecula',
+        price: {
+            current: 23000,
+            withoutDiscount: 35000
+        }
     },
     {
         id: 'chmol2v2',
@@ -261,6 +385,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chmol2v2,
         subtype: 'chair',
         model: 'molecula',
+        price: {
+            current: 23000,
+            withoutDiscount: 35000
+        }
     },
     {
         id: 'chmol2v3',
@@ -269,6 +397,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.chmol2v3,
         subtype: 'chair',
         model: 'molecula',
+        price: {
+            current: 23000,
+            withoutDiscount: 35000
+        }
     },
     {
         id: 'sbbm1',
@@ -277,6 +409,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.sbbm1,
         subtype: 'semi-bar-chair',
         model: 'magnet',
+        price: {
+            current: 20000,
+            withoutDiscount: 25000
+        }
     },
     {
         id: 'sbbm2',
@@ -285,6 +421,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.sbbm2,
         subtype: 'semi-bar-chair',
         model: 'magnet',
+        price: {
+            current: 20000,
+            withoutDiscount: 25000
+        }
     },
     {
         id: 'sbbm3',
@@ -293,6 +433,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.sbbm3,
         subtype: 'semi-bar-chair',
         model: 'magnet',
+        price: {
+            current: 20000,
+            withoutDiscount: 25000
+        }
     },
     {
         id: 'tabbm1',
@@ -301,6 +445,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm1,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm2',
@@ -309,6 +457,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm2,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm3',
@@ -317,6 +469,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm3,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm4',
@@ -325,6 +481,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm4,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm5',
@@ -333,6 +493,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm5,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm6',
@@ -341,6 +505,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm6,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 13500,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm7',
@@ -349,6 +517,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm7,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm8',
@@ -357,6 +529,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm8,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm9',
@@ -365,6 +541,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm9,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm10',
@@ -373,6 +553,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm10,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm11',
@@ -381,6 +565,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm11,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm12',
@@ -389,6 +577,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm12,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm13',
@@ -397,6 +589,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm13,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm14',
@@ -405,6 +601,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm14,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm15',
@@ -413,6 +613,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm15,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tabbm16',
@@ -421,6 +625,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tabbm16,
         subtype: 'tabouret',
         model: 'magnet',
+        price: {
+            current: 12000,
+            withoutDiscount: 14000
+        }
     },
     {
         id: 'tablines1',
@@ -429,6 +637,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines1,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines2',
@@ -437,6 +649,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines2,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines3',
@@ -445,6 +661,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines3,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines4',
@@ -453,6 +673,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines4,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 17000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines5',
@@ -461,6 +685,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines5,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines6',
@@ -469,6 +697,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines6,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines7',
@@ -477,6 +709,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines7,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'tablines8',
@@ -485,6 +721,10 @@ export const chairs: Chair[] = [
         images: CHAIRS.tablines8,
         subtype: 'tabouret',
         model: 'lines',
+        price: {
+            current: 15000,
+            withoutDiscount: 18000
+        }
     },
     {
         id: 'chalfa4',
@@ -493,5 +733,9 @@ export const chairs: Chair[] = [
         images: CHAIRS.chalfa4,
         subtype: 'chair',
         model: 'alfa',
+        price: {
+            current: 55000,
+            withoutDiscount: 74000
+        }
     }
 ]
