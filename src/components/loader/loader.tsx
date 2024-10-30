f7661d2e86b95ce006caf77eed765d54a5dc64bd
leonraz58 <leonraz58@gmail.com>
2024-10-30 16:56:33 +0300

    add Loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './loader.module.scss'
+
+export const Loader = () => {
+    return (
+        <div className={s.wrapper}>
+            <span className={s.loader}></span>
+        </div>
+    );
+};
